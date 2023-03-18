1a3a12ce836ba4236df7a92be9af1dfd3ca67263
Kateryna18 <mikolaichukkate@icloud.com>
2023-03-18 21:07:51 +0200

    added function addMarkupCountryList

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
-// 1. Напиши функцію fetchCountries(name), яка робить HTTP-запит на ресурс name 
-// і повертає проміс з масивом країн - результатом запиту. 
-// 2. Винеси її в окремий файл fetchCountries.js і зроби іменований експорт.
-
 const searchInputBox = document.querySelector('#search-box');
-// const countryList = document.querySelector('.country-list');
-// const countryInfoBox = document.querySelector('.country-info');
+const countriesList = document.querySelector('.country-list');
+const countryInfoBox = document.querySelector('.country-info');
 
 function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`).then(response => {
+    const url = `https://restcountries.com/v3/name/${name}?fields=name,capital,population,flags,languages`;
+
+    return fetch(url).then(response => {
         return response.json();
     });
 
@@ -22,32 +20,46 @@ function fetchCountries(name) {
 
 searchInputBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
 
-// 1. Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується, а розмітка списку країн 
-// або інформації про країну зникає.
 
-// 2. Виконай санітизацію введеного рядка методом trim(), це вирішить проблему, коли в полі введення тільки пробіли, 
-// або вони є на початку і в кінці рядка.
 function onSearch() {
     const nameCountry = this.value;
     fetchCountries(nameCountry).then(countries => {
-        console.log(countries);
         if(countries.length > 10) {
             alert('Too many matches found. Please enter a more specific name.')
-        } else if (countries.length > 2 || countries.length < 10) {
+        } else if (countries.length >= 2 && countries.length <= 10) {
             addMarkupCountryList(countries);
         } 
 
-        addMarkupcountryInfoBox(countries);
+        // addMarkupcountryInfoBox(countries);
+    }
+        
+        
 
-    }).catch(error => {console.log(error)})
+    ).catch(error => {console.error(error)})
 
 }
+
+
 
 function addMarkupCountryList(countries) {
+    const markupCountriesList = countries.map(country => {
+        console.log(country.name.official);
+        return `<li class="country-item">
+        <img src='' class="country-item-flag" alt="Flag" width="30">
+        <p class="country-item-name">${country.name.official}</p>
+      </li>`
+    }
+    ).join('');
+
+    
+    return countriesList.innerHTML = markupCountriesList;
 
 }
 
-function addMarkupcountryInfoBox(countries) {
+// function addMarkupcountryInfoBox(countries) {
 
-}
+// }
+
+
+
 
