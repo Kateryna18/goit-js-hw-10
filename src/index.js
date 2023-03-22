e737f846c73c76cb1ee55027bad61bd5c2cfd8eb
Kateryna18 <mikolaichukkate@icloud.com>
2023-03-22 17:51:51 +0200

    added styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 
 const DEBOUNCE_DELAY = 300;
@@ -15,31 +16,30 @@ searchInputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch() {
     const nameCountry = searchInputRef.value.trim();
+    console.log(nameCountry)
      
     
     fetchCountries(nameCountry).then(countries => {
-        if (countries.length === 0) {
-            countriesList.innerHTML = '';
-            countryInfoBox.innerHTML = '';
-        } else if (countries.length >= 2 && countries.length <= 10) {
+        if (countries.length >= 2 && countries.length <= 10) {
+            clearContent();
             addMarkupCountryList(countries);
         } else if (countries.length === 1) {
+            clearContent();
             addMarkupcountryInfoBox(countries);
         } else if (countries.length > 10) {
-            alert('Too many matches found. Please enter a more specific name.')
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } 
-    }).catch(error => {console.log(error)})
+    }).catch(error => {Notiflix.Notify.failure('Oops, there is no country with that name');
+    clearContent()})
 
 }
 
 
 function addMarkupCountryList(countries) {
     const markupCountriesList = countries.map(({name: {official}, flags}) => {
-        
-        // const flagSvg = country.flags;
-        
+       
         return `<li class="country-item">
-        <img src='${flags[0]}' class="country-item-flag" alt="Flag" width="120">
+        <img src='${flags[0]}' class="country-item-flag" alt="Flag" width="70">
         <p class="country-item-name">${official}</p>
       </li>`
     }
@@ -54,7 +54,7 @@ function addMarkupcountryInfoBox(countries) {
     const markupcountryInfoBox = countries.map(({name: {official}, flags, capital, population, languages}) => {
         const language = Object.values(languages).join(', ');
         return `<div class="country-info-head">
-         <img class = "country-info-flag" src="${flags[0]}" alt="Flag of Country" width="220">
+         <img class = "country-info-flag" src="${flags[0]}" alt="Flag of Country" width="200">
          <h2 class="country-info-tittle">${official}</h2>
        </div>
        <p class="country-info-capital"><strong>Capital: </strong>${capital}</p>
@@ -67,8 +67,9 @@ function addMarkupcountryInfoBox(countries) {
     return countryInfoBox.innerHTML = markupcountryInfoBox;
 }
 
-
-
-
+function clearContent() {
+    countriesList.innerHTML = '';
+    countryInfoBox.innerHTML = '';
+}
 
 
